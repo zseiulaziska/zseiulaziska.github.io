ae51fbd8b94d65c9a2e3c63796c26b633909342e
debowski <piotr.debowski@zseiulaziska.pl>
2026-07-20 22:33:48 +0200

    Podniesienie kontrastu

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'ZSEiU Łaziska Górne';
+export const SITE_DESCRIPTION = 'Zespół Szkół Energetycznych i Usługowych w Łaziskach Górnych';
