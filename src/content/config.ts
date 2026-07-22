b38eea718263605e5774e7da1109fea708fa8bd8
debowski <piotr.debowski@zseiulaziska.pl>
2026-07-22 14:40:43 +0200

    Lista nauczycieli

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,8 +11,23 @@ const blogSchema = z.object({
   draft: z.boolean().optional(),
 });
 
+const teacherSchema = z.object({
+  management: z.array(z.object({
+    name: z.string(),
+    role: z.string(),
+    subjects: z.array(z.string()),
+  })),
+  teachers: z.array(z.object({
+    name: z.string(),
+    subjects: z.array(z.string()),
+  })),
+});
+
 export const collections = {
   blog: defineCollection({
     schema: blogSchema,
+  }),
+  teacher: defineCollection({
+    schema: teacherSchema,
   }),
 };
